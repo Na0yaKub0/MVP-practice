a75a30b29498d8a66ee4b095e4634076fb9f389b
Na0yaKub0 <70832976+Na0yaKub0@users.noreply.github.com>
2022-01-10 17:07:51 +0900

    Update ApiService.kt

--- app/src/main/java/com/example/mvp/data/remote/api_client/ApiService.kt
+++ app/src/main/java/com/example/mvp/data/remote/api_client/ApiService.kt
@@ -7,8 +7,6 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("")
-    //"https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?applicationId=1083451209915038351&format=json&keyword="+serch+"&booksGenreId=000&sort=standard"
-    //URLで&の部分を記載する。
     fun getBooks(
         @Query("format") format:String,
         @Query("keyword") keyword:String,
